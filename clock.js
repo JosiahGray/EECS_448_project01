060aa6f311c45510ea8f8f808549f2a33a58a80a
Josiah Gray <j305g268@ku.edu>
2016-02-09 09:52:49 -0600

    Change timer() to actually increment currentTime variable instead of just constantly displaying real world time. Change increment/decrement functions to alter hours of the clock time as a test.

--- clock.js
+++ clock.js
@@ -4,15 +4,13 @@ http://www.w3schools.com/js/js_timing.asp
 var currentTime = new Date();
 var timerInterval = setInterval(timer, 1000);
 var milTime = false;
-var lastTime = currentTime.toLocaleTimeString('en-US', {hour12: !(milTime)});
-
 
 var run = true;
 
 <!-- This is a filler function to use for positioning and proof of concept -->
 function timer() {
     if (run) {
-      currentTime = new Date();
+      currentTime.setSeconds((currentTime.getSeconds() + 1));
       var options = {hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: !milTime};
       lastTime = currentTime.toLocaleTimeString('en-US', options);
       document.getElementById("time").innerHTML = lastTime;
@@ -27,10 +25,12 @@ var toggleType = function() {
 
 var incrementTime = function() {
   /*FIX ME*/
+  currentTime.setHours((currentTime.getHours() + 1)%24);
 }
 
 var decrementTime = function() {
   /*FIX ME*/
+  currentTime.setHours((currentTime.getHours() - 1)%24);
 }
 
 var set24hrs = function () {
